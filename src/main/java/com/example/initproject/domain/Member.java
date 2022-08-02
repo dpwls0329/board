6d523b9e3942272148d8c72ed816dc997e008dd1
js3322 <js@cleancode.kr>
2022-08-02 21:36:53 +0900

    init member class and jpa, ojdbc8 intall update

--- src/main/java/com/example/initproject/domain/Member.java
+++ src/main/java/com/example/initproject/domain/Member.java
@@ -1,22 +1,21 @@
 package com.example.initproject.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
     private long id;
     private String name;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
+
+//public class Member {
+//
+//}
